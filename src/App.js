73c62b4f7b27e376df7d68292582e5c6e260ab1d
Ryan McCarthy <rmmcc92@aol.com>
2021-12-01 17:41:52 -0500

    added Projects to navbar

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/project" element={<Project />} />
+            <Route path="/projects" element={<Project />} />
             <Route path="/resume" element={<Resume />} />
           </Routes>
         </PortfolioProvider>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,6 +43,9 @@ function Navbar() {
           <Link to="/" className={classes.link}>
             Home
           </Link>
+          <Link to="/projects" className={classes.link}>
+            Projects
+          </Link>
           <Link to="/about" className={classes.link}>
             About
           </Link>
